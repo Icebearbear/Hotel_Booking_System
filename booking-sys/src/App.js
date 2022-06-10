a5abf1d81091287ca7ce77d648a26c11493c1d09
Oakar Min <yuliatiyuli@gmail.com>
2022-06-10 17:32:07 +0800

    Create new react app and Login Component
    1. new booking-sys react app
    2. installed react-bootstrap for React components
    3. new component called LoginRegister

---
+++ booking-sys/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import React from "react";
+import LoginRegister from "./components/LoginRegister";
+
+function App() {
+  return (
+    <div>
+      <h1>HELLO</h1>
+      <LoginRegister />
+    </div>
+  );
+}
+
+export default App;
