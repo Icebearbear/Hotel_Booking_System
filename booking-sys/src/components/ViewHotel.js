fe65b8daffe81a158b20eeedb6a1520e401b6767
Spatarantula <98459032+Spatarantula@users.noreply.github.com>
2022-07-19 00:21:05 +0800

    fixed layout and added hotel info

--- booking-sys/src/components/ViewHotel.js
+++ booking-sys/src/components/ViewHotel.js
@@ -1,15 +1,22 @@
-import React, {useEffect, useState} from "react";
-import Card from "react-bootstrap/Card";
+import React, { useEffect, useState } from "react";
+// import Card from "react-bootstrap/Card";
+// import CardGroup from 'react-bootstrap/CardGroup';
+import { Container, Card, CardGroup, Col, Button } from 'react-bootstrap';
 import { Link, useLocation } from "react-router-dom";
-import Button from "react-bootstrap/Button";
+// import Button from "react-bootstrap/Button";
 import axios from "axios";
 
 function ViewHotel(props) {
   const location = useLocation();
   const { hotelId } = location.state; // get data passed from SearchHotelResult page
-  
   //const hotelId = "diH7";
+
   const [hotelName, setHotelName] = useState("");
+  const [address, setAddress] = useState("");
+  const [rating, setRating] = useState("");
+
+  const [descr, setDescr] = useState("");
+  const [amenities, setAmenities] = useState({});
 
   const [imageDetails, setImageDetails] = useState({});
   const [imageIndexes, setImageIndexes] = useState("");
@@ -21,39 +28,50 @@ function ViewHotel(props) {
   const [roomPrices, setRoomPrices] = useState([]);
 
   const getHotelData = () => {
-    axios.get("http://localhost:3001/viewhotel", {params: {hotelId: hotelId}}) 
-    .then((hotelData) => {
-      setHotelName(hotelData.data["name"]);
+    axios.get("http://localhost:3001/viewhotel", { params: { hotelId: hotelId } })
+      .then((hotelData) => {
+        setHotelName(hotelData.data["name"]);
+        setAddress(hotelData.data["address"]);
+        setRating(hotelData.data["rating"]);
 
-      setImageDetails(hotelData.data["image_details"])
-      setImageIndexes(hotelData.data["hires_image_index"])
+        setDescr(hotelData.data["description"]);
+        setAmenities(hotelData.data["amenities"]);
 
-      setLatitude(hotelData.data["latitude"]);
-      setLongitude(hotelData.data["longitude"]);
-    })
-    .catch((err) => console.log("hoteldata "+err.message));
+        setImageDetails(hotelData.data["image_details"]);
+        setImageIndexes(hotelData.data["hires_image_index"]);
+
+        setLatitude(hotelData.data["latitude"]);
+        setLongitude(hotelData.data["longitude"]);
+      })
+      .catch((err) => console.log("hoteldata " + err.message));
   }
 
   const imageObjConstruction = () => {
     var imageIndexList = imageIndexes.split(",");
-    imageIndexList.forEach(imageI => 
-      imageData[`${imageI}`] = imageDetails["prefix"] + imageI + imageDetails["suffix"]);   
+    imageIndexList.forEach(imageI =>
+      imageData[`${imageI}`] = imageDetails["prefix"] + imageI + imageDetails["suffix"]);
     console.log(imageData);
-    }
-
-  const getRoomsData = () => {
-    axios.get("http://localhost:3001/hotelprices", {params: {hotelId: hotelId}}) 
-    .then((hotelRoomData) => {
-      setRoomPrices(hotelRoomData.data["rooms"])
-    })
-    .catch((err) => console.log("roomdata "+err.message));
   }
+
+  const checkAmenities = (bool) => {
+    if (bool === true) {
+      return "Yes";
+    }
+    return "No";
+  }
+  // const getRoomsData = () => {
+  //   axios.get("http://localhost:3001/hotelprices", {params: {hotelId: hotelId}}) 
+  //   .then((hotelRoomData) => {
+  //     setRoomPrices(hotelRoomData.data["rooms"])
+  //   })
+  //   .catch((err) => console.log("roomdata "+err.message));
+  // }
 
   useEffect(() => {
     getHotelData();
     imageObjConstruction();
-    getRoomsData();
-    });
+    //getRoomsData();
+  });
 
   // useEffect is the first thing to load when the page is opened
   // useEffect(() => {
@@ -71,28 +89,59 @@ function ViewHotel(props) {
   // });
 
   return (
-  <>
-    <div className="d-flex justify-content-around">
-      <Card style={{ width: "90rem", height: "30rem" }}>
-        <Card.Body>
-          <h1>View Selected Hotel Page</h1>
+    <>
+      <Container maxWidth="lg" className='p-4'>
+        {/* Intro Section */}
+        <div className="d-flex justify-content-around">
+          <Card style={{ flex: 1 }}>
+            <Card.Body>
+              <h1 class="card-title">{hotelName}</h1>
+              <address>{address}</address>
+              <h6>{"Hotel id: " + hotelId}</h6>
+              <div className="e-card">
+                <div className="e-card-image">
+                  <Card.Img style={{ flex: 0.5 }} src={"https://d2ey9sqrvkqdfs.cloudfront.net/050G/1.jpg"} />
+                </div>
+              </div>
+            </Card.Body>
+          </Card>
+        </div>
 
-          <h4>{"Hotel id: " + hotelId + " is selected"}</h4>
-          <h4>{"Hotel name: " + hotelName}</h4>
-          <h4>{"what" + roomPrices[0]}</h4>
-          <img src={"https://d2ey9sqrvkqdfs.cloudfront.net/050G/1.jpg"} />
-          <Link className="link" to="/custinfo" state={{ hotelId: hotelId }}>
-            <Button variant="primary" type="submit" className="float-right">
-              Book hotel
-            </Button>
-          </Link>
-        </Card.Body>
-      </Card>
-    </div>
-    <div className='actions'>
-      <button className='btn' onClick={imageObjConstruction}>view pic</button>
-    </div>
-  </>
+        {/* Hotel description */}
+        <CardGroup>
+          <Card style={{ flex: 2 }}>
+            <Card.Body>
+              <h2>Hotel Overview</h2>
+              <Card.Text dangerouslySetInnerHTML={{ __html: descr }} />
+            </Card.Body>
+          </Card>
+          <Card style={{ flex: 1 }}>
+            <Card.Body>
+              <h3>Amenities</h3>
+              <Card.Text class="text-justify">
+                {Object.entries(amenities).map(([key, value]) => (
+                  <Card.Text>{key + ": " + checkAmenities(value)}</Card.Text>
+                ))}
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </CardGroup>
+
+        {/* <div className='actions'>
+          <Card style={{ width: "90rem", height: "30rem" }}>
+            <Card.Body>
+              <Link className="link" to="/custinfo" state={{ hotelId: hotelId }}>
+                <Button variant="primary" type="submit" className="float-right">
+                  Book hotel
+                </Button>
+              </Link>
+              <button className='btn' onClick={imageObjConstruction}>view pic</button>
+            </Card.Body>
+          </Card>
+        </div> */}
+
+      </Container>
+    </>
   );
 };
 
@@ -106,14 +155,14 @@ function ViewHotel(props) {
 //     <>
 //       {props.details.map((value, index) => (
 //         <div className='relative flex items-center'>
-        
+
 //         </div>
 //       ))};
-      
+
 //         {imageData.props.map((item) => (
 //           <img src={item.img} />
 //         ))}
-      
+
 //     </>
 //     );
 // }
