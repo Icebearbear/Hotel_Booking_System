7c1a7629aa9d21601d2107fe09201c4ff187a23f
Spatarantula <98459032+Spatarantula@users.noreply.github.com>
2022-07-21 17:23:32 +0800

    change viewhotel to check if hires_image_index exists
    prevent page not load from split() used on undefined

--- server/index.js
+++ server/index.js
@@ -84,14 +84,23 @@ app.get("/viewhotel", (req, res) => {
       .get(`https://hotelapi.loyalty.dev/api/hotels/${hotelId}`)
       .then((hotelres) => {
         console.log("From API: " + hotelId);
-        const ids = hotelres.data.hires_image_index;
-        const imgId = ids.split(",");
         const imgDet = hotelres.data.image_details;
+        const ids = hotelres.data.hires_image_index;
+
         const imgUrl = [];
-        imgId.forEach(
-          (imageI) =>
-            (imgUrl[`${imageI}`] = imgDet["prefix"] + imageI + imgDet["suffix"])
-        );
+        if (typeof ids !== 'undefined') {
+          const imgId = ids.split(",");
+          imgId.forEach(
+            (imageI) =>
+              (imgUrl[`${imageI}`] = imgDet["prefix"] + imageI + imgDet["suffix"])
+          );
+        }
+        else{
+          for (let i = 0; i < hotelres.data.number_of_images; i++) {
+            imgUrl[`${i}`] = imgDet["prefix"] + i + imgDet["suffix"]
+          }
+            
+        }
 
         res.status(200).json({
           data: JSON.stringify(hotelres.data),
@@ -170,7 +179,7 @@ app.get("/hotelidprices", (req, res) => {
     axios
       .get(url)
       .then((roomres) => {
-        console.log("got SPECIFIC HOTEL room prices " + roomres.data);
+        console.log("got SPECIFIC HOTEL room prices ")
         res.status(200);
         res.send(roomres.data); //returned data is in roomprices.data and send it to react frontend
       })
