f5bd71a9f4e01f4432447dbeb2ecddf8e189737c
victoriachong <victoria_chong@mymail.sutd.edu.sg>
2022-07-26 20:39:48 +0800

    Merge branch 'feature-2-cmi' of https://github.com/Icebearbear/Hotel_Booking_System into feature-2-cmi

--- __tests__/feature1.test.js
+++ __tests__/feature1.test.js
@@ -1,4 +1,19 @@
-const request = require("supertest");
-const baseURL = "http://localhost:3001";
-const server = require("../server/index");
+// const { async } = require("@firebase/util");
+// const request = require("supertest");
+// const { describe } = require("yargs");
+// const baseURL = "http://localhost:3001";
+// const server = require("../server/index");
+// import { hasUncaughtExceptionCaptureCallback } from "process";
+const newFilter = require("./feature1test.js")
 
+describe("test handle filter",()=> {
+    
+    it("should return subset of " , async() =>{
+        var first = newFilter("sin");
+        console.log(first);
+        var second = newFilter("sing");
+        var subset = second.some(r=> first.includes(r));
+        console.log(subset);
+        expect(subset).toBe(true);
+    })
+})
